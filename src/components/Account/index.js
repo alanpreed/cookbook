fcfe665aed69d07cafc4ae9c7f5a050436f922cc
Alan Reed <alan_p_reed@hotmail.co.uk>
2019-11-23 20:43:13 +0000

    Implemented Firebase auth following tutorial

---
+++ src/components/Account/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class AccountPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>AccountPage</h1>
+      </div>
+    );
+  }
+}
+
+export default AccountPage;
