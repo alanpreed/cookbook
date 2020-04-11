069fe130df3a1667c0a0d5fbfab5b4f09e449a04
Alan Reed <alan_p_reed@hotmail.co.uk>
2020-04-11 17:55:58 +0100

    Added authorisation protection to routes

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import { withAuthorisation } from '../Session';
 
 class HomePage extends React.Component {
   render() {
     return (
       <div>
         <h1>HomePage</h1>
+        <p> This page is accessible by every signed in user.</p>
       </div>
     );
   }
 }
 
-export default HomePage;
+const condition = authUser => authUser != null;
+
+export default withAuthorisation(condition)(HomePage);
