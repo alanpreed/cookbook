0e58841204524d65d11e461235c78a828623db3a
Alan Reed <alan_p_reed@hotmail.co.uk>
2020-04-12 10:28:27 +0100

    Added user specific information to account page

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -2,14 +2,20 @@ import React from 'react'
 
 import { PasswordForgetForm } from '../PasswordForget';
 import PasswordChangeForm from '../PasswordChange';
-import { withAuthorisation } from '../Session';
+import { AuthUserContext, withAuthorisation } from '../Session';
+import { auth } from 'firebase';
 
 const AccountPage = () => (
-    <div>
-      <h1>Account Page</h1>
-      <PasswordForgetForm />
-      <PasswordChangeForm />
-    </div>
+  <AuthUserContext.Consumer>
+    {authUser =>  (
+      <div>
+        <h1>Account: {authUser.email}</h1>
+        <h2>UID: {authUser.uid}</h2>
+        <PasswordForgetForm />
+        <PasswordChangeForm />
+      </div>
+    )}
+  </AuthUserContext.Consumer>
 );
 
 const condition = authUser => authUser != null;
